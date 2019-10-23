4fa73ad205c1dea327ef109db23399daf3a3d078
Lucy Murray <lucy.murray-kagool@outlook.com>
2019-10-23 14:44:02 +0100

    remove retail work

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HelloWorld from "./containers/HelloWorld.vue";
-import Arrows from "./containers/Arrows.vue";
-import Animations from "./containers/MoreAnimations.vue";
-import MeterInputV2 from "./containers/MeterInputV2.vue";
-import KeyActions from "./components/KeyActions.vue";
-import ContentScroll from "./containers/ContentScroll.vue";
-import ContentScrollScss from "./containers/ContentScrollScss.vue";
-import GetLocation from './containers/GetLocation.vue';
 import DowTriagePoc from "./components/DowTriagePoc.vue";
 import DowTriagePocNext from "./components/DowTriagePocNext.vue";
 import Cases from "./components/Cases.vue";
@@ -20,20 +12,6 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/arrows",
-    component: Arrows
-  },
-  {
-    path: "/animations",
-    component: Animations
-  },
-  { path: "/meter-reading", component: HelloWorld },
-  { path: "/meter-reading-v2", component: MeterInputV2 },
-  { path: "/content-scroll", component: ContentScroll },
-  { path: "/key-actions", component: KeyActions },
-  { path: "/content-scroll-scss", component: ContentScrollScss },
-  { path: '/get-location', component: GetLocation},
   { path: '/dow-triage-poc', component: DowTriagePoc},
   { path: '/dow-triage-poc-next', name: 'dow-triage-poc-next', component: DowTriagePocNext},
   { path: '/cases', name: 'cases', component: Cases},
